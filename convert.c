f81bd2a27715f099a6a9972da69db6a22cd66cd9
Tom <elunico@users.noreply.github.com>
2022-11-13 18:05:50 -0500

    formatting for mac

--- convert.c
+++ convert.c
@@ -37,44 +37,36 @@ static const char *help_fmt =
     "  arrays (26*26) but with the -e option you can have up to \n"
     "  208,827,064,576 arrays (26^8)\n";
 
-struct options
-{
+struct options {
   int padded : 1;
   int reversed : 1;
   int extended : 1;
 };
 
-void usage()
-{
+void usage() {
   fprintf(stderr, "%s\n", usage_string);
   exit(1);
 }
 
-void try_increment(char **sptr, int index)
-{
+void try_increment(char **sptr, int index) {
   char *string = *sptr;
-  if (index < 0)
-  {
+  if (index < 0) {
     fprintf(stderr, "Too Many Increments!\n");
     abort();
   }
   string[index]++;
-  if (string[index] > 'z')
-  {
+  if (string[index] > 'z') {
     try_increment(sptr, index - 1);
     string[index] = 'a';
   }
 }
 
-struct options get_options(int *argc, char **argv[])
-{
+struct options get_options(int *argc, char **argv[]) {
   char ch;
   struct options opts = {0};
   int count = 0;
-  while ((ch = getopt(*argc, *argv, "prhe")) != -1)
-  {
-    switch (ch)
-    {
+  while ((ch = getopt(*argc, *argv, "prhe")) != -1) {
+    switch (ch) {
     case 'p':
       opts.padded = 1;
       count++;
@@ -100,26 +92,22 @@ struct options get_options(int *argc, char **argv[])
   return opts;
 }
 
-int atoip(struct options opts, int scount, char *strings[])
-{
+int atoip(struct options opts, int scount, char *strings[]) {
   static char const *const DIGIT_FMT = "%02x";
   int maxlen = opts.extended ? 8 : 2;
   char *static_ident = opts.extended ? "aaaaaaaa" : "aa";
 
   char *ident = strdup(static_ident); // don't mutate a static string
-  for (int i = 0; i < scount; i++)
-  {
+  for (int i = 0; i < scount; i++) {
     char *convertee = strings[i];
     int length = strlen(convertee); // need the NUL byte
     int blocks = length / INT_SIZE;
     int extra = length % INT_SIZE;
 
     printf("int %s[] = {", ident);
-    for (int i = 0; i < blocks; i++)
-    {
+    for (int i = 0; i < blocks; i++) {
       printf("0x");
-      for (int j = 0; j < INT_SIZE; j++)
-      {
+      for (int j = 0; j < INT_SIZE; j++) {
         int block_offset = !opts.reversed ? INT_SIZE - j - 1 : j;
         printf(DIGIT_FMT, convertee[i * INT_SIZE + block_offset]);
       }
@@ -127,15 +115,13 @@ int atoip(struct options opts, int scount, char *strings[])
         printf(", ");
     }
 
-    if (extra)
-    {
+    if (extra) {
       printf("0x");
       if (opts.padded && !opts.reversed)
         for (int i = 0; i < INT_SIZE - extra; i++)
           printf("00");
 
-      for (int i = 0; i < extra; i++)
-      {
+      for (int i = 0; i < extra; i++) {
         int block_offset = !opts.reversed ? extra - i - 1 : i;
         printf(DIGIT_FMT, convertee[length - extra + (block_offset)]);
       }
@@ -143,9 +129,7 @@ int atoip(struct options opts, int scount, char *strings[])
       if (opts.reversed)
         for (int i = 0; i < INT_SIZE - extra; i++)
           printf("00");
-    }
-    else
-    {
+    } else {
       printf(", 0x0");
     }
 
@@ -156,13 +140,12 @@ int atoip(struct options opts, int scount, char *strings[])
   free(ident);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
   if (argc < 2)
     usage();
 
   struct options opts = get_options(&argc, &argv);
 
-  atoip(opts, argc-1, (char**)(argv + 1));
+  atoip(opts, argc - 1, (char **)(argv + 1));
 }
